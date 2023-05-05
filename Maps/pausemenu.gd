8cff7ab77737d4a1b9d23c40ed3d8a169d9f8849
asier <asiergonzalez@elpuig.xeill.net>
2023-05-05 17:55:34 +0200

    menu pausa

---
+++ Maps/pausemenu.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@onready var pausemenu = $pausemenu
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pausemenu.visible = true
+		get_tree().paused = true
+
+
+func _on_resume_button_pressed():
+	pausemenu.visible = false
+	get_tree().paused = false
+
+
+
+	
