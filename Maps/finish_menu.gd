10e4079b6f3209848a361256f1a3a767fcd975b9
asier <asiergonzalez@elpuig.xeill.net>
2023-05-21 17:22:58 +0200

    cambios

--- Maps/finish_menu.gd
+++ Maps/finish_menu.gd
@@ -1,16 +1,17 @@
 extends Node
 
 func _ready():
+	$Timer.start()
 	Global.vidas = 5
 	Save.game_data.VidasJugador = 5
 	Save.save_data()
 	$AnimCerdito.play("cerdito")
 	$AnimRhino.play("rhino")
 	$AnimSnail.play("snail")
-	$AnimPiedra.play("piedra")
+	$AnimationPlayer.play("pink")
 	$AnimPlanta.play("planta")
 	$AnimTortuga.play("tortuga")
 
 
-func _on_button_pressed():
+func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://Maps/main_menu.tscn")
